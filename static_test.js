f5c5a188b5840a7f65d09d0dfa3eb65a8a7d452b
toru-shirai <toru-shirai@kccs.co.jp>
2023-11-20 16:42:49 +0900

    修正

---
+++ static_test.js
@@ -0,0 +1,9 @@
+function alertButton(){
+    var newElement = document.createElement("p"); // p要素作成
+    var newContent = document.createTextNode("子要素３"); // テキストノードを作成
+    newElement.appendChild(newContent); // p要素にテキストノードを追加
+    newElement.setAttribute("id","child-p3"); // p要素にidを設定
+
+    var parentDiv = document.getElementById("main");
+    parentDiv.appendChild(newElement);
+}
